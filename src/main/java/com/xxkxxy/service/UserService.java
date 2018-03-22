1f3d303b31ce6e752cd5c82af7415ecae07a78ed
LK <307832539@qq.com>
2018-03-22 16:17:10 +0800

    初始化完成

---
+++ src/main/java/com/xxkxxy/service/UserService.java
@@ -0,0 +1,32 @@
+package com.xxkxxy.service;
+
+import com.xxkxxy.dao.UserDao;
+import com.xxkxxy.entity.User;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Resource
+    private UserDao userDao;
+
+    public int insert(User pojo){
+        return userDao.insert(pojo);
+    }
+
+    public int insertList(List< User> pojos){
+        return userDao.insertList(pojos);
+    }
+
+    public List<User> select(User pojo){
+        return userDao.select(pojo);
+    }
+
+    public int update(User pojo){
+        return userDao.update(pojo);
+    }
+
+}
