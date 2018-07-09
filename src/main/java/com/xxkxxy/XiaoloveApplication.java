3402a363b765bd07a0f1fc07e3372c1ec7826494
LK <li.kang@siemens.com>
2018-07-09 16:54:18 +0800

    tomcat 外部部署

--- src/main/java/com/xxkxxy/XiaoloveApplication.java
+++ src/main/java/com/xxkxxy/XiaoloveApplication.java
@@ -3,9 +3,16 @@ package com.xxkxxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class XiaoloveApplication {
+public class XiaoloveApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(XiaoloveApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(XiaoloveApplication.class, args);
